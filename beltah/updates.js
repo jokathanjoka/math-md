97a0a76f30fa49be29ed23a1dc72978ad9250ed9
FredieTech <frediezra60@gmail.com>
2024-07-07 09:03:28 +0300

    Update updates.js

--- beltah/updates.js
+++ beltah/updates.js
@@ -4,10 +4,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "wagroup", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join 𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗 WhatsApp Testing Group\n\n';
+    let z = 'Hello 👋\n\nClick The link below to Join MATH MD WhatsApp Testing Group\n\n';
     let d = 'https://chat.whatsapp.com/CQvUnqaDK4fBjgMXvFEAsI';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
+    var img = 'https://telegra.ph/file/0bc8a3f6e4e684addb141.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -15,10 +15,10 @@ console.log("mon test");
 
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join 𝗕𝗘𝗟𝗧𝗔𝗛 𝗠𝗗 WhatsApp Channel\n\n';
-    let d = 'https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F';
+    let z = 'Hello 👋\n\nClick The link below to Join MATH 𝗠𝗗 WhatsApp Channel\n\n';
+    let d = 'https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg';
+    var img = 'https://telegra.ph/file/0bc8a3f6e4e684addb141.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
